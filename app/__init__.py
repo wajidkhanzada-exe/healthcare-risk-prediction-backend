70ba7809bfe2f8b97351fd9a0f3f48ec3f2c72c3
wajid khanzada <khaliqrehman952@gmail.com>
2026-09-22 11:39:38 +0500

    Allow production frontend CORS

--- app/__init__.py
+++ app/__init__.py
@@ -105,7 +105,7 @@ def create_app():
             "http://localhost:5173",
             "http://127.0.0.1:5173",
             "https://healthcare-risk-prediction-frontend.vercel.app",
-            "https://healthriskkk-ai.vercel.app/"
+            "https://healthriskkk-ai.vercel.app"
         ]
     )
 
